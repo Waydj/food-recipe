97e129c5d11f20fcc75c224dae0f9b34b4054e2a
Waydj <farhminait@gmail.com>
2023-10-23 16:20:47 +0200

    create welcome screen, home screen (30%)

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text, ScrollView, Image, TextInput } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { BellIcon } from "react-native-heroicons/outline";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/images/avatar.png")}
+            style={{ width: hp(5), height: hp(5) }}
+          />
+          <BellIcon size={hp(4)} color="gray" />
+        </View>
+
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{ fontSize: hp(1.7) }} className="text-neutral-600">
+            Hello, Alex!
+          </Text>
+          <View>
+            <Text
+              style={{ fontSize: hp(3.8) }}
+              className="font-semibold text-neutral-600"
+            >
+              Make your own food,
+            </Text>
+          </View>
+          <Text
+            style={{ fontSize: hp(3.8) }}
+            className="font-semibold text-neutral-600"
+          >
+            stay at <Text className="text-amber-400">home</Text>
+          </Text>
+        </View>
+
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          <TextInput
+            placeholder="Search any recipe"
+            placeholderTextColor="gray"
+            style={{ fontSize: hp(1.7) }}
+            className="flex-1 text-base mb-1 pl-3 tracking-wider"
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
